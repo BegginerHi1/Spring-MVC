ba16d16dae8fda5fa5da6b58cf109d635c3c463b
BegginerHi1 <146478622+BegginerHi1@users.noreply.github.com>
2023-12-12 15:15:50 +0300

    Update Car.java

--- src/main/java/web/Model/Car.java
+++ src/main/java/web/Model/Car.java
@@ -4,6 +4,7 @@ public class Car {
     private int id;
     private String model;
     private int series;
+
     public Car(int id, String model, int series) {
         this.id = id;
         this.model = model;
