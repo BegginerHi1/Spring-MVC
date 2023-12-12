df6ba98685883ce038c29d1d67ccff9cf9e2af1a
BegginerHi1 <146478622+BegginerHi1@users.noreply.github.com>
2023-12-12 15:18:25 +0300

    Update CarController.java

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,32 +4,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import web.Model.Car;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.Service.CarService;
 import web.Service.CarServiceImpl;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
     private CarService carService = new CarServiceImpl();
-    private List<Car> list = CarServiceImpl.getCarList();
 
     @GetMapping
-    public String Cars(Model model, HttpServletRequest request) {
-        int count = 0;
+    public String Cars(Model model,
+                       @RequestParam(value = "count", required = false) Integer count) {
+
+        model.addAttribute("list", CarServiceImpl.getCarList());
+        System.out.println("В случае если пользователь не ввёл параметры, то просто" +
+                "добавляем в model список всех машин");
+
+        System.out.println("----------------------------------------------------------");
+
+        System.out.println("Внизу будет попытка извлечь значение count из параметра" +
+                "И вставить это значение в метод listByCount");
         try {
-            count = Integer.parseInt(request.getParameter("count"));
-        } catch (NumberFormatException e) {
-            e.getCause();
-        }
-        if (count == 0) {
-            model.addAttribute("list", list);
-        } else {
-            List<Car> listByCount = carService.listByCount(list, count);
-            model.addAttribute("listByCount", listByCount);
+            model.addAttribute("list",
+                    carService.listByCount(CarServiceImpl.getCarList(), count));
+        } catch (NullPointerException e) {
+            System.out.println("Выше идёт проверка, ввёл ли пользователь параметры," +
+                    "иначе методу listByCount нечего будет принять на место числа," +
+                    "от того и выскочит ошибка");
         }
         return "cars";
     }
