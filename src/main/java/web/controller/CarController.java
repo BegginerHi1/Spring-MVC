ac19eb29933af37ee142593c9ef264e36ec649e7
BegginerHi1 <146478622+BegginerHi1@users.noreply.github.com>
2023-12-13 16:28:39 +0300

    Update CarController.java

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,28 +1,26 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.Service.CarService;
-import web.Service.CarServiceImpl;
+
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
-    private CarService carService = new CarServiceImpl();
+
+    @Autowired
+    private CarService carService;
 
     @GetMapping
-    public String Cars(Model model,
-                       @RequestParam(value = "count", required = false) Integer count) {
-        model.addAttribute("list", CarServiceImpl.getCarList());
-        try {
-            model.addAttribute("list",
-                    carService.listByCount(CarServiceImpl.getCarList(), count));
-        } catch (NullPointerException e) {
-            e.fillInStackTrace();
-        }
+    public String listByCount(@RequestParam(value = "count", required = false) Integer count,
+                              Model model) {
+        model.addAttribute("listByCount", carService.listByCount(count));
         return "cars";
     }
 }
+
